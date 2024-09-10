f572c0cbfff65fd5795a64cfe0356abdc193effa
Ricardo Castellon <cesar_castellon@hotmail.com>
2024-09-10 16:02:22 -0600

    Primer commit para la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { CustomSideNavComponent } from './components/custom-sidenav/custom-sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    CustomSideNavComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-youtube-sidebar';
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed( () => this.collapsed() ? '65px' : '250px' );
 }
